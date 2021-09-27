67d1b5fb21c55d1d768ae43199fe4d98fca00c5d
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-09-27 20:35:51 +0200

    Made H# weekopdracht

---
+++ myportfolio/php_website/mediaplayer.php
@@ -0,0 +1,19 @@
+<?php
+    include_once 'header.php';
+    ?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <div class="global">
+            <div class="playlist">
+
+
+
+
+            </div>
+        </div>
+    </body>
+</html>
+<?php
+    include_once 'footer.php';
+    ?>
