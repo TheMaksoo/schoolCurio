24400e1ac09e7bcf38e8308480e24d4e85106f1e
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-10-20 23:51:46 +0200

    opdracht 5

---
+++ python/vscodeIntro/main.py
@@ -0,0 +1,10 @@
+getal1 = input("voer getal 1 in:")
+getal2 = input("voer getal 2 in:")
+
+getal1 = int(getal1)
+getal2 = int(getal2)
+
+result = getal1 + getal2
+
+for i in range(result + 1):
+    print(i)
