2097514c8b5452e6fabfb240bb10301c0c505729
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-10-21 12:03:56 +0200

    word counter

--- python/song/main.py
+++ python/song/main.py
@@ -1,5 +1,18 @@
 songinfo = open("song\song.txt", "r")
-songtext = songinfo.read()
+output = open("song\output.txt", "w")
 
-for line in songtext:
+linecount = 0
+wordcount = 0
+for line in songinfo:
     print(line, end="")
+    linecount += 1
+    words = len(line.split())
+    wordcount += words
+    output.write(f"{words} | {line}")
+
+print("--------------")
+print(f"Lines: {linecount}")
+print(f"Words: {wordcount}")
+
+songinfo.close()
+output.close()
