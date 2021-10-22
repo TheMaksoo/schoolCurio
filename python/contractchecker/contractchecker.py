1db1cc66ab3ab5fe0de348bc69ff936b1fb43c69
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-10-22 15:08:58 +0200

    search

--- python/contractchecker/contractchecker.py
+++ python/contractchecker/contractchecker.py
@@ -4,6 +4,8 @@ contractFile = open("contractchecker\\contracts.csv", "r", encoding="UTF-8")
 contractDict = csv.DictReader(contractFile)
 contractList = list(contractDict)
 
+isFound = False
+
 os.system("cls")
 print("1 = Statistieken\n 2 = ContractChecker")
 choice = input("\nMaak een keuze: 1 of 2:\n")
@@ -14,7 +16,19 @@ if choice == "1":
     print(f"aantal contracten: {count}")
 
 elif choice == "1":
-    pass
+    id = input("Voer een contract-nummer in: ")
+
+    for contract in contractList:
+        if contract["contract_id"] == id:
+            isFound = True
+            break
+
+    os.system("cls")
+
+    if isFound:
+        print(f"{contract['contract_id']} | {contract['company']}")
+    else:
+        print(f"Contract met nummer {id} niet gevonden!")
 
 
 contractFile.close()
