c5aed81ac0ca8045b73e5bda5a10b1e8920a85fc
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2021-11-30 10:45:58 +0100

    C#

--- CSharp/lesVandaag/Program.cs
+++ CSharp/lesVandaag/Program.cs
@@ -3,34 +3,40 @@ DateTime now  = DateTime.Now;
 string day = now.DayOfWeek.ToString();
 string invoer = day;
 string uitvoer = "";
+string dag = "";
 
 switch (invoer)
 {
 	case "Monday":
 		{
+			dag = "maandag";
 			uitvoer = "C# lessen";
 			break;
 		}
 	case "Tuesday":
 		{
+			dag = "dinsdag";
 			uitvoer = "Project";
 			break;
 		}
 	case "Wednesday":
 		{
+			dag = "woensdag";
 			uitvoer = "Keuzedeel";
 			break;
 		}
 	case "Thursday":
 		{
+			dag = "donderdag";
 			uitvoer = "HTLM-CSS";
 			break;
 		}
 	case "Friday":
 		{
+			dag = "vrijdag";
 			uitvoer = "Python";
 			break;
 		}
 }
-Console.WriteLine("Op " + invoer + " heb je " + uitvoer + ".");
+Console.WriteLine("Op " + dag + " heb je " + uitvoer + ".");
 Console.ReadKey();
