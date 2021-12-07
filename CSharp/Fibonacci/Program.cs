c0ad4f76b708093e6d1b487847f1d56c8ef212ed
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2021-12-07 08:48:05 +0100

    C#

---
+++ CSharp/Fibonacci/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 1;
+int b = 0;
+int c = 0;
+for (int i = 0; i < 25; i++)
+{
+    c = a + b;
+    Console.WriteLine(c);
+    a = b;
+    b = c;
+}
---
+++ CSharp/nummers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Numbers 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] numbers = {10, 12, 13, 15};
+            int[] numbers2 = { -12, -10, -5, 0, 1, 6, 8, 10 };
+            double[] brokenNumbers = { 16.45, 18.98, 897.78, 12.11, 78976.78 };
+            Console.ReadKey();
+        }
+    }
+}
