6645bb6106a3ad52f4fbb3e86c4c6ae2616255f6
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2022-02-14 14:30:08 +0100

    C#

--- CSharp/FilmApplicatie/Form1.Designer.cs
+++ CSharp/FilmApplicatie/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FilmApplicatie
 {
-    partial class dgvFilms
+    partial class form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,35 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgvFilms = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilms)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(175, 91);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 0;
-            // 
             // dgvFilms
+            // 
+            this.dgvFilms.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFilms.Location = new System.Drawing.Point(184, 93);
+            this.dgvFilms.Name = "dgvFilms";
+            this.dgvFilms.Size = new System.Drawing.Size(240, 150);
+            this.dgvFilms.TabIndex = 0;
+            // 
+            // form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
-            this.Name = "dgvFilms";
+            this.Controls.Add(this.dgvFilms);
+            this.Name = "form";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilms)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvFilms;
     }
 }
 
--- CSharp/FilmApplicatie/Form1.cs
+++ CSharp/FilmApplicatie/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FilmApplicatie
 {
-    public partial class dgvFilms : Form
+    public partial class form : Form
     {
-        public dgvFilms()
+        public form()
         {
             InitializeComponent();
         }
@@ -20,6 +20,21 @@ namespace FilmApplicatie
         private void Form1_Load(object sender, EventArgs e)
         {
             string[,] films = { { "Star Wars: Episode IV: A New Hope", "1977","SciFi" }, { "Aliens", "1986", "Horror"}, { "The Silence of the Lambs", "1991", "Thriller" }, { "Die Hard", "1988","Actie "}, { "The Green Mile", "1999", "Drama" }, { "Avatar", "2009", "SciFi" }, { "Avenger: Infinity War", "2018", "Actie" } };
+            int aantalRijen = films.GetLength(0);
+            int aantalKolommen = films.GetLength (1);
+
+            dgvFilms.ColumnCount = aantalKolommen;
+
+            for (int r = 0; r < aantalRijen; r++)
+            {
+                DataGridViewRow dataRij = new DataGridViewRow();
+                dataRij.CreateCells(dgvFilms);
+
+                for (int k = 0; k < aantalKolommen; k++)
+                {
+                    dataRij.Cells[k].Value = films[r, k];
+                }
+            }
         }
     }
 }
