cf3bf1d5e2ac6d330f87204d72b01f7948a55c94
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2022-02-15 14:24:12 +0100

    C#

---
+++ CSharp/HelloWorld/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace MiljonairsQuiz
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string question = "Jouw antwoord?";
+            Console.WriteLine("Welkom bij de Miljonairs Quiz!");
+            Console.WriteLine("Vraag 1");
+            Console.WriteLine("Wat is C#\n" + "a. Spaans bedrijf\n" + "b. Programmeertaal\n" + "c. Computer\n" + "d. Website op het internet\n");
+            Console.WriteLine(question);
+            string answer;
+            answer = Console.ReadLine();
+
+            if (answer != "b")
+            {
+                wrongAwnser();
+            }
+           
+                
+            Console.WriteLine("Vraag 2");
+            Console.WriteLine("Hoe vergelijk je een programmeertaal\n" + "a. Loop\n" + "b. Funcite\n" + "c. If-statement\n" + "d. Methode\n");
+            Console.WriteLine(question);
+            answer = Console.ReadLine();
+
+            if (answer != "c")
+            {
+                wrongAwnser();
+            }
+           
+
+            Console.WriteLine("Vraag 3");
+            Console.WriteLine("Hoe herhaal je in een programmeertaal\n" + "a. Loop\n" + "b. Funcite\n" + "c. Herhaal\n" + "d. Return\n");
+            Console.WriteLine(question);
+            answer = Console.ReadLine();
+
+            if (answer != "a")
+            {
+                wrongAwnser();
+            }
+            
+
+            Console.WriteLine("Vraag 4");
+            Console.WriteLine("Wie was een hoofdachitect bij de ontwikkeling van C#\n" + "a. Bill Gates\n" + "b. Tim Lutt\n" + "c. Anders Heijsberg\n" + "d. Steve Jobs\n");
+            Console.WriteLine(question);
+            answer = Console.ReadLine();
+
+            if (answer != "c")
+            {
+                wrongAwnser();
+            }
+           
+
+            Console.WriteLine("Vraag 5");
+            Console.WriteLine("Wat kun je niet met C# maken\n" + "a. Mobiele apps\n" + "b. Desktoapplicaties\n" + "c. Webapplicaties\n" + "d. Frietje met mayo om op te eten\n");
+            Console.WriteLine(question);
+            answer = Console.ReadLine();
+
+            if (answer != "d")
+            {
+                wrongAwnser();
+            }
+           
+
+            Console.WriteLine("Goed gedaan! Je hebt alle vragen goed beantwoord :)");
+            Thread.Sleep(5000);
+
+            static async void wrongAwnser()
+            {
+                Console.WriteLine("Dat is helaas niet het goede antwoord!");
+                Console.WriteLine();
+                Console.WriteLine("De Quiz sluit nu af...");
+                Thread.Sleep(500);
+                System.Environment.Exit(1);
+            }
+            
+        }
+    }
+
+
+
+}
