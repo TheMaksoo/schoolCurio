1c9884020dfafdf0a6646e4155f79fcb3fd04556
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2022-02-24 13:46:36 +0100

    Failed
    Failed to get mollie api runnin.

---
+++ scoutingshop/index.php
@@ -0,0 +1,198 @@
+    <?php include('partials-front/menu.php'); ?>
+
+    <!-- fOOD sEARCH Section Starts Here -->
+    <section class="food-search text-center">
+        <div class="container">
+            
+            <form action="<?php echo SITEURL; ?>/food-search.php" method="POST">
+                <input type="search" name="search" placeholder="Search merch" required>
+                <input type="submit" name="submit" value="Search" class="btn btn-primary">
+            </form>
+
+        </div>
+    </section>
+    <!-- fOOD sEARCH Section Ends Here -->
+
+    <?php 
+        if(isset($_SESSION['order']))
+        {
+            echo $_SESSION['order'];
+            unset($_SESSION['order']);
+        }
+
+        /*
+        * Make sure to disable the display of errors in production code!
+        */
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+        
+        require_once __DIR__ . "\autoload.php";
+        require_once __DIR__ . "\\functions.php";
+
+        /*
+        * Initialize the Mollie API library with your API key.
+        *
+        * See: https://www.mollie.com/dashboard/developers/api-keys
+        */
+
+        
+    ?>
+s
+    <!-- CAtegories Section Starts Here -->
+    <section class="categories">
+        <div class="container">
+            <h2 class="text-center">Explore Various Merch Categories</h2>
+
+            <?php 
+                //Create SQL Query to Display CAtegories from Database
+                $sql = "SELECT * FROM tbl_category WHERE active='Yes' AND featured='Yes' ORDER BY id DESC LIMIT 3";
+                //Execute the Query
+                $res = mysqli_query($conn, $sql);
+                //Count rows to check whether the category is available or not
+                $count = mysqli_num_rows($res);
+
+                if($count>0)
+                {
+                    //CAtegories Available
+                    while($row=mysqli_fetch_assoc($res))
+                    {
+                        //Get the Values like id, title, image_name
+                        $id = $row['id'];
+                        $title = $row['title'];
+                        $image_name = $row['image_name'];
+                        ?>
+                        
+                        <a href="<?php echo SITEURL; ?>/category-foods.php?category_id=<?php echo $id; ?>">
+                            <div class="box-3 float-container">
+                                <?php 
+                                    //Check whether Image is available or not
+                                    if($image_name=="")
+                                    {
+                                        //Display MEssage
+                                        echo "<div class='error'>Image not Available</div>";
+                                    }
+                                    else
+                                    {
+                                        //Image Available
+                                        ?>
+                                        <img src="<?php echo SITEURL; ?>/images/category/<?php echo $image_name; ?>"  class="img-responsive img-curve">
+                                        <?php
+                                    }
+                                ?>
+                                
+
+                                <h3 class="float-text text-white" ><mark style="background-color:white;"><?php echo $title; ?></mark></h3>
+                            </div>
+                        </a>
+
+                        <?php
+                    }
+                }
+                else
+                {
+                    //Categories not Available
+                    echo "<div class='error'>Category not Added.</div>";
+                }
+            ?>
+
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Categories Section Ends Here -->
+
+
+
+    <!-- fOOD MEnu Section Starts Here -->
+    <section class="food-menu">
+        <div class="container">
+            <h2 class="text-center">Our Merch</h2>
+
+            <?php 
+            
+            //Getting Foods from Database that are active and featured
+            //SQL Query
+            $sql2 = "SELECT * FROM tbl_merch WHERE active='Yes' AND featured='Yes' LIMIT 6";
+
+            //Execute the Query
+            $res2 = mysqli_query($conn, $sql2);
+
+            //Count Rows
+            $count2 = mysqli_num_rows($res2);
+
+            //CHeck whether food available or not
+            if($count2>0)
+            {
+                //Food Available
+                while($row=mysqli_fetch_assoc($res2))
+                {
+                    //Get all the values
+                    $id = $row['id'];
+                    $title = $row['title'];
+                    $price = $row['price'];
+                    $description = $row['description'];
+                    $image_name = $row['image_name'];
+                    ?>
+
+                    <div class="food-menu-box">
+                        <div class="food-menu-img">
+                            <?php 
+                                //Check whether image available or not
+                                if($image_name=="")
+                                {
+                                    //Image not Available
+                                    echo "<div class='error'>Image not available.</div>";
+                                }
+                                else
+                                {
+                                    //Image Available
+                                    ?>
+                                    <img src="<?php echo SITEURL; ?>/images/food/<?php echo $image_name; ?>" alt="" class="img-responsive img-curve">
+                                    <?php
+                                }
+                            ?>
+                            
+                        </div>
+
+                        <div class="food-menu-desc">
+                            <h4><?php echo $title; ?></h4>
+                            <p class="food-price">$<?php echo $price; ?></p>
+                            <p class="food-detail">
+                                <?php echo $description; ?>
+                            </p>
+                            <br>
+
+                            <a href="<?php echo SITEURL; ?>/order.php?food_id=<?php echo $id; ?>" class="btn btn-primary">Order Now</a>
+                        </div>
+                    </div>
+
+                    <?php
+                }
+            }
+            else
+            {
+                //Food Not Available 
+                echo "<div class='error'>Food not available.</div>";
+            }
+            
+            ?>
+
+            
+
+ 
+
+            <div class="clearfix"></div>
+
+            
+
+        </div>
+
+        <p class="text-center">
+            <a href="#">See All merch</a>
+        </p>
+    </section>
+    <!-- fOOD Menu Section Ends Here -->
+
+    
+    <?php include('partials-front/footer.php'); ?>
