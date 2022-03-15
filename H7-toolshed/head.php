be25477e59d1e8d3e333fec9e7ef04475f55ce9d
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2022-03-15 11:38:15 +0100

    Add files via upload

---
+++ H7-toolshed/head.php
@@ -0,0 +1,6 @@
+<?php require_once 'backend/config.php'; ?>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/H7-Toolshed/css/normalize.css">
+<link rel="stylesheet" href="/H7-Toolshed/css/main.css">
---
+++ H7-toolshed/header.php
@@ -0,0 +1,10 @@
+<?php require_once 'backend/config.php'; ?>
+
+<header>
+    <div class="container">
+        <nav>
+            <a href="/H7-Toolshed//index.php">Home</a> |
+            <a href="/H7-Toolshed//tools/index.php">Tools</a>
+        </nav>
+    </div>
+</header>
